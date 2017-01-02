aa2093bafc08b978c39eaebf67f802f23216244b
Pawel Mateja <p0mateja@gmail.com>
2017-01-02 15:47:59 +0100

    Name changed to ftpfind

---
+++ ftpfind.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import ftplib
+import netrc
+import argparse
+
+
+def main(args):
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[:])
